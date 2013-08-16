3c67c2a35c7b313eddaea83333d1e1232b115351
Ville Korhonen <ville@xd.fi>
2013-08-16 17:46:26 +0300

    Added simple script to start KVM & others

---
+++ webvm_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+websockify 2001 localhost:2000 > /dev/null 2> /dev/null < /dev/null &
+websockify_pid=$!
+
+SPICE_DEBUG_ALLOW_MC=1 kvm -drive file=debian_wheezy_amd64_desktop.qcow2,cache=none,index=0,if=virtio -snapshot -net nic,model=virtio -net user -m 2048 -cpu host -spice port=2000,password=w > /dev/null 2> /dev/null < /dev/null &
+kvm_pid=$!
+
+ssh -N -R2001:localhost:2001 nm.0xff.fi > /dev/null 2> /dev/null < /dev/null &
+ssh_pid=$!
+
+echo $websockify_pid
+echo $kvm_pid
+echo $ssh_pid
