afc2238aff6971a647015384b9be6d69d0e53066
Ville Korhonen <ville@xd.fi>
2013-08-16 17:22:02 +0300

    Use argparse

--- furry-shame.py
+++ furry-shame.py
@@ -9,14 +9,21 @@ import os
 import sys
 import json
 import requests
+import argparse
 
+WEBVM_API_HOST = 'nm.0xff.fi'
 WEBVM_API_VERSION = "v1"
-WEBVM_API_BASE = "http://%(host)s/api/%(version)s"
+WEBVM_API_PROTO = 'https'
+WEBVM_API_BASE = "%(proto)s://%(host)s/api/%(version)s"
+
+# TODO: By default, use HTTPS
 
 def main(args):
     api_url = WEBVM_API_BASE % {
-        'host': 'nm.0xff.fi',
+        'proto': WEBVM_API_PROTO,
+        'host': args.host,
         'version': WEBVM_API_VERSION,
+        
     }
     
     
@@ -24,7 +31,12 @@ def main(args):
     return 0
 
 def run():
-    args = []
+    parser = argparse.ArgumentParser(
+        description='WebVM Slave Client',
+    )
+    parser.add_argument('--host', dest='host', help='API server', default=WEBVM_API_HOST)
+    
+    args = parser.parse_args()
     sys.exit(main(args))
 
 if __name__ == "__main__":
