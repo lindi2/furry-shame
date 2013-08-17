c73e594ddee7ae5e09b80e28b078abe13ea5239f
Timo Juhani Lindfors <timo.lindfors@iki.fi>
2013-08-17 16:34:47 +0300

    Add example for running digabi livecd

--- webvm_test.sh
+++ webvm_test.sh
@@ -4,6 +4,7 @@ websockify 2001 localhost:2000 > /dev/null 2> /dev/null < /dev/null &
 websockify_pid=$!
 
 SPICE_DEBUG_ALLOW_MC=1 kvm -drive file=debian_wheezy_amd64_desktop.qcow2,cache=none,index=0,if=virtio -snapshot -net nic,model=virtio -net user -m 2048 -cpu host -spice port=2000,password=w > /dev/null 2> /dev/null < /dev/null &
+#SPICE_DEBUG_ALLOW_MC=1 kvm -drive file=digabi-livecd-v1.0.iso,cache=none,index=0,media=cdrom -snapshot -net nic -net user -m 2048 -cpu host -spice port=2000,password=w > /dev/null 2> /dev/null < /dev/null &
 kvm_pid=$!
 
 ssh -N -R2001:localhost:2001 nm.0xff.fi > /dev/null 2> /dev/null < /dev/null &
